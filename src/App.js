32fd26fa24af13619b9807c0aad2111f564e7dfd
Rygel-XVI <micha232@umn.edu>
2019-03-07 20:18:23 -0800

    added routes for create and modify

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 import Nav from './containers/Nav'
 import IngredientListContainer from './containers/IngredientListContainer'
 import RecipeDetails from './components/RecipeDetails'
-
+import CreateRecipeForm from './components/CreateRecipeForm'
+import ModifyRecipeForm from './components/ModifyRecipeForm'
 
 class App extends Component {
   render() {
@@ -17,6 +18,8 @@ class App extends Component {
             <Route exact path="/" component={IngredientListContainer} />
             <Route exact path="/search" component={IngredientListContainer} />
             <Route path="/recipes/:id/:name" render={(props) => <RecipeDetails recipe={props} />} />
+            <Route path="/recipes/create" render={CreateRecipeForm} />
+            <Route path="/recipes/update" render={ModifyRecipeForm} />
           </React.Fragment>
         </Router>
       </div>
