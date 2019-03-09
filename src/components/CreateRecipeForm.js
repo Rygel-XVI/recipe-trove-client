33dc479105b847b88dfe3fccb05d0d2a5e90fc33
Rygel-XVI <micha232@umn.edu>
2019-03-08 16:08:57 -0800

    added local state for CreateRecipeForm

--- src/components/CreateRecipeForm.js
+++ src/components/CreateRecipeForm.js
@@ -7,6 +7,16 @@ class CreateRecipeForm extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {
+      recipe: {
+        name: '',
+        description: '',
+        instructions: '',
+        rating: -1,
+        servings: ''
+      }
+    }
+
     this.handleSubmit.bind(this)
   }
 
@@ -18,7 +28,10 @@ class CreateRecipeForm extends Component {
   render() {
     return (
       <div>
-      Create
+      <form onSubmit={this.handleSubmit}>
+      <input text=
+      <button className='create-recipe' type="submit">Create Recipe</button>
+      </form>
       </div>
     )
   }
