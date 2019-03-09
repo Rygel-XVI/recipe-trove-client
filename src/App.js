c6ac4e0f85552f91ef2ee8609903d8bad664316c
Rygel-XVI <micha232@umn.edu>
2019-03-08 16:35:01 -0800

    changed name of IngredientListContainer to a more accurate SearchDisplayContainer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
 
 import Nav from './containers/Nav'
-import IngredientListContainer from './containers/IngredientListContainer'
+import SearchDisplayContainer from './containers/SearchDisplayContainer'
 import RecipeDetails from './components/RecipeDetails'
 import CreateRecipeForm from './components/CreateRecipeForm'
 import ModifyRecipeForm from './components/ModifyRecipeForm'
@@ -15,8 +15,8 @@ class App extends Component {
         <Router>
           <React.Fragment>
             <Nav />
-            <Route exact path="/" component={IngredientListContainer} />
-            <Route exact path="/search" component={IngredientListContainer} />
+            <Route exact path="/" component={SearchDisplayContainer} />
+            <Route exact path="/search" component={SearchDisplayContainer} />
             <Route path="/recipes/:id/:name" render={(props) => <RecipeDetails recipe={props} />} />
             <Route exact path="/recipes/create" component={CreateRecipeForm} />
             <Route exact path="/recipes/update" component={ModifyRecipeForm} />
