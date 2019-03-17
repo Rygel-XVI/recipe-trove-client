25869822dfebb8f6fadbed0a3a0951b61230d305
Rygel-XVI <micha232@umn.edu>
2019-03-17 13:24:48 -0700

    changed it so recipe list title only shows up if the ingredient is in recipes

--- src/components/IngredientDetails.js
+++ src/components/IngredientDetails.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
-const IngredientDetails = ({ ingredient }) => (
-  <div className="ingredient-details">
-    <h4>{ingredient.description}</h4>
-    <ul>
-    Recipes:
-    {ingredient.recipes.map(recipe => <li>{recipe.name}</li>)}
-    </ul>
-  </div>
-)
+const IngredientDetails = ({ ingredient }) => {
+  if (ingredient.recipes.length > 0) {
+    return (
+      <div>
+      <h4>{ingredient.description}</h4>
+      <ul>
+        Recipes:
+        {ingredient.recipes.map(recipe => <li>{recipe.name}</li>)}
+        </ul>
+        </div>
+      )
+    } else {
+      return (
+        <h4>{ingredient.description}</h4>
+      )
+    }
+  }
+
 export default IngredientDetails
