b7c2e4c7e98ea2ddf7d7164ef12364465aa29df0
Rygel-XVI <micha232@umn.edu>
2019-03-17 12:45:40 -0700

    added appropriate ingredient actions

--- src/actions/ingredients.js
+++ src/actions/ingredients.js
@@ -25,3 +25,38 @@ console.log(JSON.stringify({ ingredient }))
     .catch(err => console.log(err))
   }
 }
+
+export const updateIngredient = (ingredient) => {
+  let data = {
+    method: 'PATCH',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ ingredient })
+  }
+
+  return dispatch => {
+    fetch(baseUrl + "/" + ingredient.id, data)
+    .then(resp => resp.json())
+    .then((ingredient) => dispatch({ type: 'UPDATE_INGREDIENT', ingredient }))
+    .catch(err => console.log(err))
+  }
+}
+
+export const deleteIngredient = (id) => {
+  let data = {
+    method: 'DELETE',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }
+  }
+
+  return dispatch => {
+    fetch(baseUrl + "/" + id, data)
+    .then(resp => resp.json())
+    .then((ingredient) => dispatch({ type: 'DELETE_INGREDIENT', ingredient }))
+    .catch(err => console.log(err))
+  }
+}
